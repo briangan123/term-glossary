75ab1b4682a147856abb4ebaad3c2efbf5f07456
Brian <briangan157@gmail.com>
2022-04-05 13:12:56 -0400

    updated src files

--- src/add-term-form.js
+++ src/add-term-form.js
@@ -1,14 +1,26 @@
-export default function updateData() {
-    console.log("here");
-    const term = document.querySelector("#term").value;
-    const definition = document.querySelector("#definition").value;
-    const context = document.querySelector("#context").value;
-    const url = new URL('https://term-glossary.vercel.app/api/addterm.js')
-    url.searchParams.set('term',term)
-    url.searchParams.set('definition', definition)
-    url.searchParams.set('context', context)
+class updateData {
 
-    fetch(`/api/addTerm?term= &definition= &context=` + term + definition + context).then(res => res.json()).then((data) => {
-     document.querySelector("#term").value = JSON.stringify(data, null, 2);
-    });
-  }
+    static get properties() {
+		return {
+			term: {type: String},
+			definition: {type: String},
+			context: {type: String}
+        }
+    }
+
+    static updateData() {
+        console.log("here");
+        const term = document.querySelector("#term").value;
+        const definition = document.querySelector("#definition").value;
+        const context = document.querySelector("#context").value;
+        const url = new URL('https://term-glossary.vercel.app/api/addterm.js')
+        url.searchParams.set('term',term)
+        url.searchParams.set('definition', definition)
+        url.searchParams.set('context', context)
+    
+        fetch(`/api/addTerm?term= ${term} `).then(res => res.json()).then((data) => {
+         document.querySelector("#term").value = JSON.stringify(data, null, 2);
+        });
+      }
+}
+customElements.define('add-term-form', updateData);
