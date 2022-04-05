e153d8ff37f36d0512605639196e5ea473276e62
brt5181 <97695371+brt5181@users.noreply.github.com>
2022-04-05 12:36:28 -0400

    src changes

---
+++ src/add-term-form.js
@@ -0,0 +1,14 @@
+export default function updateData() {
+    console.log("here");
+    const term = document.querySelector("#term").value;
+    const definition = document.querySelector("#definition").value;
+    const context = document.querySelector("#context").value;
+    const url = new URL('https://term-glossary.vercel.app/api/addterm.js')
+    url.searchParams.set('term',term)
+    url.searchParams.set('definition', definition)
+    url.searchParams.set('context', context)
+
+    fetch(`/api/addTerm?term= &definition= &context=` + term + definition + context).then(res => res.json()).then((data) => {
+     document.querySelector("#term").value = JSON.stringify(data, null, 2);
+    });
+  }
