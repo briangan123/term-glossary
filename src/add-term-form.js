8a2d6f55f8114fde9b77fd0bc88e1649716207e0
Brian <briangan157@gmail.com>
2022-04-06 22:09:17 -0400

    worked on frontend

--- src/add-term-form.js
+++ src/add-term-form.js
@@ -1,38 +1,43 @@
 
-class updateData extends HTMLFormElement{
+// class updateData extends HTMLFormElement{
 
-  constructor() {
-    super();
-    this.term = "";
-    this.definition = "";
-    this.context = "";
-  }
+//   constructor() {
+//     super();
+//     this.term = "";
+//     this.definition = "";
+//     this.context = "";
+//   }
 
-    static get properties() {
-		return {
-			term: {type: String},
-			definition: {type: String},
-			context: {type: String}
-        }
-    }
+//     static get properties() {
+// 		return {
+// 			term: {type: String},
+// 			definition: {type: String},
+// 			context: {type: String}
+//         }
+//     }
 
 
-}
+// }
 
 const form = document.querySelector("#add-term")
-form.addEventListener("submit", () => {
+form.addEventListener("submit", (e) => {
+e.preventDefault()
+  // const term = document.querySelector("#term").value;
+  // const definition = document.querySelector("#definition").value;
+  // const context = document.querySelector("#context").value;
+  const term = form.term.value;
+  const definition = form.definition.value;
+  const context = form.context.value;
 
-  const term = document.querySelector("#term").value;
-  const definition = document.querySelector("#definition").value;
-  const context = document.querySelector("#context").value;
   const url = new URL('https://term-glossary.vercel.app/api/addterm.js')
-  url.searchParams.set('term',term)
+  url.searchParams.set('term', term)
   url.searchParams.set('definition', definition)
   url.searchParams.set('context', context)
-
-  fetch(`/api/addTerm`).then(res => res.json()).then((data) => {
-   document.querySelector("#term").value = JSON.stringify(data, null, 2);
-  });
+  console.log(term)
+  console.log(url)
+  // fetch(`/api/addTerm`).then(res => res.json()).then((data) => {
+  //  document.querySelector("#term").value = JSON.stringify(data, null, 2);
+  // });
 
 }
 )
