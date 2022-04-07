ab7a734170d4a321d86ca00e7551c403725c71d0
Brian <briangan157@gmail.com>
2022-04-07 12:47:47 -0400

    connected addTerm to planetscale

--- src/add-term-form.js
+++ src/add-term-form.js
@@ -22,24 +22,18 @@
 const form = document.querySelector("#add-term")
 form.addEventListener("submit", (e) => {
 e.preventDefault()
-  // const term = document.querySelector("#term").value;
-  // const definition = document.querySelector("#definition").value;
-  // const context = document.querySelector("#context").value;
+
   const term = form.term.value;
   const definition = form.definition.value;
   const context = form.context.value;
 
-  const url = new URL('https://term-glossary.vercel.app/api/addterm.js')
-  url.searchParams.set('term', term)
-  url.searchParams.set('definition', definition)
-  url.searchParams.set('context', context)
   console.log(term)
   console.log(definition)
   console.log(context)
-  console.log(url)
-  // fetch(`/api/addTerm`).then(res => res.json()).then((data) => {
-  //  document.querySelector("#term").value = JSON.stringify(data, null, 2);
-  // });
+
+  fetch(`/api/addTerm.js?term=${term}&definition=${definition}&context=${context}`).then(res => res.json()).then((data) => {
+   console.log(data);
+  });
 
 }
 )
