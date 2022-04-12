f570907ec73a87cc641831fd7780b4bca2ee7723
Brian <briangan157@gmail.com>
2022-04-12 13:24:20 -0400

    updates

--- src/term-list.js
+++ src/term-list.js
@@ -6,13 +6,55 @@ class termList extends LitElement{
     }
     
     render() {
-        return html`stuff`;
+        return html`
+        <style>
+            table, th, td {
+            border:1px solid black;
+            }
+        </style>
+        <body>
+            <h2>Glossary</h2>
+            <table style=“width:100%“>
+            <tr>
+                <th>Term</th>
+                <th>Definition</th>
+                <th>Context</th>
+            </tr>
+            
+            <tr>
+                <td></td>
+                <td>‘first def’</td>
+                <td>‘context’</td>
+            </tr>
+            <tr>
+                <td>‘2nd term’</td>
+                <td>‘2nd def’</td>
+                <td>‘context’</td>
+            </tr>
+            </table>
+        </body>
+        `;
     }
-    async getData() {
-        await fetch(`../api/viewList.js`).then(res => res.json()).then((data) => {
+
+    async getList() {
+        await fetch(`api/viewList.js`).then(res => res.json()).then((data) => {
             console.log(data);
-            this.terms = data;
+            this.listMap = data;
+            return data;
         });
     }
 }
-customElements.define('term-List', termList);
+customElements.define('term-list', termList);
+
+// need a static get properties for data
+// use     ${this.view === 'list'
+// ? html`
+// <ul>
+//   ${this.image.map(
+//     item => html`
+//       <li>
+//       ${item.image} - ${item.title} - ${item.description} - ${item.secondary_creator}
+//       </li>
+//     `
+//   )}
+// </ul>
